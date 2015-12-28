e9c7a6e3bbd7ebc46d67c8241cd4b7db79828e4b
webrtcgame <ge.unitel@gmail.com>
2015-12-28 21:22:41 +0000

    comment

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //github test
 
 /*
@@ -22,90 +21,79 @@ http.createServer(function(req, res) {
     Keep a global list of currently connected clients
     -----------------------------------------------
 */
-var User = function User(nick){
-Object.call(this,nick);
-this.nick = nick;
-this.guid = this.generateGUID();
-this.level = this.generateLevel();
-var _hosting = false;
-Object.defineProperty(this, "hosting", {
-    get: function() {
-      return _hosting;
-    },
-    set: function(value) {
-        console.log("hosting: " + value);
-      this._hosting = value;
-    },
-    enumerable: true,
-    configurable: true
-  });
-var _clientOf = false;
-Object.defineProperty(this, "clientOf", {
-    get: function() {
-      return _clientOf;
-    },
-    set: function(value) {
-        console.log("ClientOf: " + value);
-      this._clientOf = value;
-    },
-    enumerable: true,
-    configurable: true
-  });
-  var _exp = 0;
-Object.defineProperty(this, "exp", {
-    get: function() {
-      return _exp;
-    },
-    set: function(value) {
-        console.log("exp: " + value);
-      this._exp = value;
-      this.level = this.generateLevel();
-      console.log("Level " + this.level);
-    },
-    enumerable: true,
-    configurable: true
-  });
-  this.exp = 400;
-  
+var User = function User(nick) {
+    Object.call(this, nick);
+    this.nick = nick;
+    this.guid = this.generateGUID();
+
+    this.hosting = false;
+
+    var _clientOf = false;
+    Object.defineProperty(this, "clientOf", {
+        get: function() {
+            return _clientOf;
+        },
+        set: function(value) {
+            console.log("ClientOf: " + value);
+            this._clientOf = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    var _exp = 0;
+    Object.defineProperty(this, "exp", {
+        get: function() {
+            return _exp;
+        },
+        set: function(value) {
+            console.log("exp: " + value);
+            this._exp = value;
+            this.level = this.generateLevel();
+            console.log("Level " + this.level);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    this.exp = 400;
+    this.level = this.generateLevel(this.exp);
 };
 
 User.prototype = Object.create(Object.prototype);
-User.prototype.generateGUID = function generateGUID(){
-    
+User.prototype.generateGUID = function generateGUID() {
+
     var s4 = function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     };
 
-function guid() {
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-};
+    function guid() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    };
 
-return guid();
+    return guid();
 
 };
-User.prototype.generateLevel = function generateLevel(varexp){
+User.prototype.generateLevel = function generateLevel(varexp) {
     var v1 = Math.sqrt(varexp) - 4;
     return Math.sqrt(v1) + 1;
 };
-User.prototype.host = function host(){
-    if (!this.hosting){
+User.prototype.host = function host() {
+    if (!this.hosting) {
         this.hosting = true;
     }
 };
-User.prototype.join = function join(clientGuid){
-    if (!this.hosting){
+User.prototype.join = function join(clientGuid) {
+    if (!this.hosting) {
         this.clientOf = clientGuid;
     }
 };
-User.prototype.endClient = function endClient(){};
-User.prototype.generateClientURL = function generateClientURL(){
-  return "?mode=client&guid=" + this.clientOf + "&username=" + this.nick;
+User.prototype.endClient = function endClient() {};
+User.prototype.generateClientURL = function generateClientURL() {
+    return "?mode=client&guid=" + this.clientOf + "&username=" + this.nick;
 };
-User.prototype.generateHostURL = function generateHostURL(){
-  return "?mode=host&guid=" + this.guid + "&username=" + this.nick;
+User.prototype.generateHostURL = function generateHostURL() {
+    return "?mode=host&guid=" + this.guid + "&username=" + this.nick;
 };
-User.prototype.regenerateGuid = function regnerateGuid(){
+User.prototype.regenerateGuid = function regnerateGuid() {
     this.guid = this.generateGUID();
 };
 
@@ -149,40 +137,50 @@ io.sockets.on("connection", function(socket) {
         -------------------------------------
     */
     socket.on('join', function(nick, callback) {
-var avaliable = true;
-for (var i = 0; i < userlist.length; i++) {
-if (userlist[i].nick === nick){
-avaliable = false;
-    }
-}
-if (avaliable){
-socket.user = new User(nick);
-//console.log(JSON.stringify(socket.user));
+        var avaliable = true;
+        for (var i = 0; i < userlist.length; i++) {
+            if (userlist[i].nick === nick) {
+                avaliable = false;
+            }
+        }
+        if (avaliable) {
+            socket.user = new User(nick);
+            //console.log(JSON.stringify(socket.user));
             userlist.push(socket.user);
             socket.broadcast.emit("user-joined", socket.user);
-            socket.broadcast.emit('updateusers',userlist);
+            socket.broadcast.emit('updateusers', userlist);
             callback(true, userlist);
-} else {
-    callback(false);
-}
+        }
+        else {
+            callback(false);
+        }
 
     });
 
-    
+
     /*
         Handle chat messages
         --------------------
     */
     socket.on("chat", function(message) {
         if (socket.user.nick && message) {
-            io.sockets.emit("chat", {sender: socket.user.nick, message: message});
+            io.sockets.emit("chat", {
+                sender: socket.user.nick,
+                message: message
+            });
         }
     });
-    
-socket.on("hosting",function(usernick){
-    socket.user.host();
-   io.sockets.emit("hosting",usernick); 
-});
+    socket.on("userJoinedGame", function(message) {
+        // socket.emit("userJoinedGame",{usernick:socket.user.nick,joinedTo:user.nick});
+        console.log("********************************************");
+        io.sockets.emit("userJoinGameReceived", message.usernick, message.joinedTo);
+    });
+    socket.on("hosting", function(usernick) {
+
+        socket.user.hosting = true;
+
+        io.sockets.emit("hosting", usernick);
+    });
 
     /*
         Handle client disconnection
@@ -191,20 +189,20 @@ socket.on("hosting",function(usernick){
     socket.on("disconnect", function() {
         // Check that the user has already joined successfully
         if (socket.user.nick) {
-           
-    for(var i in userlist){
-        if(userlist[i].nick===socket.user.nick){
-            userlist.splice(i,1);
-            break;
+
+            for (var i in userlist) {
+                if (userlist[i].nick === socket.user.nick) {
+                    userlist.splice(i, 1);
+                    break;
+                }
             }
-    }
 
             // Remove the client from the global list
             //clients.splice(clients.indexOf(socket.nick), 1);
             //userlist.splice(userlist.indexOf(socket.user),1);
-            
+
             // Let all the remaining clients know of the disconnect
-            io.sockets.emit('updateusers',userlist);
+            io.sockets.emit('updateusers', userlist);
             io.sockets.emit("user-left", socket.user);
         }
     });
